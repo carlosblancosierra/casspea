76083696c40b48824050d36e8e31242bd14ec156
carlosblancomarketing <carlosblancosierra@gmail.com>
2022-12-26 16:04:15 +0100

    Feat: update order emails.py

--- orders/emails.py
+++ orders/emails.py
@@ -1,58 +1,67 @@
 from django.template.loader import render_to_string, get_template
 from django.core.mail import send_mail, mail_managers
 from .models import Order
-# from jimmiss.settings.production import EMAIL_STATIC_URL
-#
-#
-# def nueva_orden_mail_staff(order_id):
-#     qs = Order.objects.filter(order_id=order_id)
-#     if qs.exists():
-#         order = qs.first()
-#         context = {"order": order, "STATIC_URL": EMAIL_STATIC_URL}
-#         try:
-#             subject = 'JimMiss - Nueva Orden'
-#             message = render_to_string('mails/orders/nueva_staff.txt', context)
-#             html_message = render_to_string('mails/orders/nueva_staff.html', context)
-#             to_mails = ['jimmiss.sistemas@gmail.com']
-#             from_mail = 'no-responda@jimmiss.mx'
-#
-#             staff_mails_sent = send_mail(
-#                 subject=subject,
-#                 message=message,
-#                 from_email=from_mail,
-#                 recipient_list=to_mails,
-#                 fail_silently=False,
-#                 html_message=html_message
-#             )
-#
-#             return staff_mails_sent
-#         except:
-#             pass
-#
-#
-# def nueva_orden_mail_client(order_id):
-#     qs = Order.objects.filter(order_id=order_id)
-#     if qs.exists():
-#         order = qs.first()
-#         context = {"order": order, "STATIC_URL": EMAIL_STATIC_URL}
-#         try:
-#             subject = 'Tu orden de JimMiss.mx'
-#             message = render_to_string('mails/orders/nueva_cliente.txt', context)
-#             html_message = render_to_string('mails/orders/nueva_cliente.html', context)
-#             to_mails = [order.user.email]
-#             from_mail = 'no-responda@jimmiss.mx'
-#
-#             staff_mails_sent = send_mail(
-#                 subject=subject,
-#                 message=message,
-#                 from_email=from_mail,
-#                 recipient_list=to_mails,
-#                 fail_silently=False,
-#                 html_message=html_message
-#             )
-#
-#             return staff_mails_sent
-#         except:
-#             pass
-#     else:
-#         pass
+from django.conf import settings
+from ebdjango.settings.production import EMAIL_STATIC_URL
+
+
+def new_order_staff_mail(order_id):
+    qs = Order.objects.filter(order_id=order_id)
+    if qs.exists():
+        order = qs.first()
+        context = {
+            "order": order,
+            "STATIC_URL": EMAIL_STATIC_URL,
+            "address": order.shipping_address,
+            "entries": order.cart_entries,
+            "gift_message": order.gift_message,
+            "shipping_cost": order.shipping_cost,
+        }
+
+        try:
+            subject = 'CassPea.co.uk | New Order'
+            message = render_to_string('mails/orders/new_order_staff.txt', context)
+            html_message = render_to_string('mails/orders/new_order_staff.html', context)
+            to_mails = ['casspea.sistemas@gmail.com']
+            from_mail = 'CassPea <info@casspea.co.uk>'
+
+            staff_mails_sent = send_mail(
+                subject=subject,
+                message=message,
+                from_email=from_mail,
+                recipient_list=to_mails,
+                fail_silently=False,
+                html_message=html_message
+            )
+
+            return staff_mails_sent
+        except:
+            pass
+
+
+def new_order_client_mail(order_id):
+    qs = Order.objects.filter(order_id=order_id)
+    if qs.exists():
+        order = qs.first()
+        context = {"order": order, "STATIC_URL": EMAIL_STATIC_URL}
+        try:
+            subject = 'Your CassPea.co.uk order'
+            message = render_to_string('mails/orders/new_client.txt', context)
+            html_message = render_to_string('mails/orders/new_client.html', context)
+            to_mails = [order.user.email]
+            from_mail = 'CassPea <info@casspea.co.uk>'
+
+            staff_mails_sent = send_mail(
+                subject=subject,
+                message=message,
+                from_email=from_mail,
+                recipient_list=to_mails,
+                fail_silently=False,
+                html_message=html_message
+            )
+
+            return staff_mails_sent
+        except:
+            pass
+    else:
+        pass
