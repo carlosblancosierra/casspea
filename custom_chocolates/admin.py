78ea0da2539726b920e693aee9f0c03010cf8b2c
carlosblancomarketing <carlosblancosierra@gmail.com>
2023-02-27 03:21:52 +0100

    Feat: add ChocolateDesignLayerType to admin view

--- custom_chocolates/admin.py
+++ custom_chocolates/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from .models import ChocolateDesignLayer, ChocolateDesign, UserChocolateDesign, ChocolateDesignBase
+from .models import \
+    ChocolateDesignLayerType, \
+    ChocolateDesignLayer, \
+    ChocolateDesignBase, \
+    UserChocolateDesign, \
+    ChocolateDesign
 
 # Register your models here.
+admin.site.register(ChocolateDesignLayerType)
 admin.site.register(ChocolateDesignLayer)
 admin.site.register(ChocolateDesignBase)
-
 admin.site.register(UserChocolateDesign)
-
 
 class ChocolateDesignModelAdmin(admin.ModelAdmin):
     filter_horizontal = (
-                         'layer1_options',
-                         'layer2_options',
-                         'layer3_options',
-                         'base_options',
-                         )
-
-
+        'layer1_options',
+        'layer2_options',
+        'layer3_options',
+        'base_options',
+    )
 admin.site.register(ChocolateDesign, ChocolateDesignModelAdmin)
