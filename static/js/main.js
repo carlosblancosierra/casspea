8a30693d6f2a4092ccb440760d9c6a4375570b0a
carlosblancomarketing <carlosblancosierra@gmail.com>
2023-07-01 20:50:33 +0200

    Feat: accesibility improvements

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,25 @@
 var owlProduct;
 
 $(document).ready(function () {
-      let owlProduct = $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsive:{
-            0:{
-                items:1
-            }
-        },
-        mouseDrag: true,
-        dotsContainer: '#box-carousel-dots'
-    })
+    let owlProduct = $('.owl-carousel');
 
-    setTimeout(function() {
+    if (owlProduct.length > 0) {
+        owlProduct.owlCarousel({
+            loop: true,
+            margin: 10,
+            responsive: {
+                0: {
+                    items: 1
+                }
+            },
+            mouseDrag: true,
+            dotsContainer: '#box-carousel-dots'
+        });
+    }
+
+    setTimeout(function () {
         var popup = document.getElementById('popup');
         popup = M.Modal.init(popup);
-        popup.open()
+        popup.open();
     }, 2000); // Adjust the delay (in milliseconds) as per your requirement
 });
