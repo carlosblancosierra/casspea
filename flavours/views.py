4be02cf7d458ede34fedfa14e64f18791bcebca8
carlosblancomarketing <carlosblancosierra@gmail.com>
2023-07-03 12:39:27 +0200

    Style: flavours page

--- flavours/views.py
+++ flavours/views.py
@@ -7,9 +7,13 @@ from .models import Flavour
 def home_page(request):
     qs = Flavour.objects.active()
 
+    split_qs = [qs[i:i + 3] for i in range(0, qs.count(), 3)]
+    print(split_qs)
+
     context = {
         "title": 'Our Flavours',
         "qs": qs,
+        "split_qs": split_qs,
     }
 
     return render(request, "flavours/home.html", context)
