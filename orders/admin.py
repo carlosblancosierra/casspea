21dd56f89a216d03b8ac1acde5f0f7ca6ed4e126
carlosblancomarketing <carlosblancosierra@gmail.com>
2023-08-24 12:28:47 +0200

    Feat: admin order list update

--- orders/admin.py
+++ orders/admin.py
@@ -5,13 +5,15 @@ from .models import Order
 # Register your models here.
 
 class OrderModelAdmin(admin.ModelAdmin):
-    list_display = ["order_id", "user", "status", "total", "timestamp"]
+    list_display = ["order_id", "user", "shipping_address_name", "payment_status", "shipping_date", "discount", "total", "timestamp", "staff_email_sent"]
 
     search_fields = ["order_id"]
     list_filter = ["user", "status", "timestamp"]
 
     readonly_fields = ['cart_entries']
 
+    def shipping_address_name(self, obj):
+        return obj.shipping_address.full_name
 
     class Order:
         model = Order
