e03fdd730e67fb87e78ebb72a805000bd7f555ef
Carlos Blanco <carlosblancosierra@gmail.com>
2023-10-25 15:46:38 +0200

    Fix: add advent qs to store view

--- store/views.py
+++ store/views.py
@@ -18,6 +18,11 @@ def home_page(request):
         "qs": qs,
     }
 
+    advent_calendar_qs = BoxSize.objects.filter(slug="Advent-Calendar")
+    if advent_calendar_qs.exists():
+        print(advent_calendar_qs.first())
+        context["advent_calendar_obj"] = advent_calendar_qs.first()
+
     return render(request, "store/home.html", context)
 
 
