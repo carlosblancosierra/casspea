608d5b795af98cdb037c852c9d185f753d540813
Carlos Blanco <carlosblancosierra@gmail.com>
2023-12-11 20:21:48 +0100

    Feat: Calendar view v2

--- dashboards/views.py
+++ dashboards/views.py
@@ -49,19 +49,26 @@ def week_orders_page(request):
     orders_qs = Order.objects.filter(payment_status="paid")
 
     today = datetime.now()
-    date_range = [today - timedelta(days=i) for i in range(7)]
+    week_offset = int(request.GET.get('week_offset', 1))
+
+    start_date = today - timedelta(days=(7 * week_offset))
+
+    date_range = [start_date - timedelta(days=i) for i in range(7)]
 
     recent_orders_by_day = {}
 
     for date in date_range:
         day_orders = orders_qs.filter(updated__date=date.date())
         day = {
-            "orders": list(day_orders)
+            "orders": list(day_orders),
+            "day_of_week": date.strftime('%A'),  # Add the day of the week
+            "order_count": len(day_orders)
         }
         recent_orders_by_day[date.date()] = day
 
     context = {
-        "recent_orders_by_day": recent_orders_by_day
+        "recent_orders_by_day": recent_orders_by_day,
+        "current_week_offset": week_offset,
     }
 
     return render(request, "dashboards/calendar.html", context)
