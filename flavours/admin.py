33a71dc08bcb68e1514adfa040a0e7178446ab9c
Carlos Blanco <carlosblancosierra@gmail.com>
2024-01-23 13:33:32 +0100

    Feat: Flavour admin list

--- flavours/admin.py
+++ flavours/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Flavour, PreBuildFlavour, Allergen
 
-# Register your models here.
-admin.site.register(Flavour)
+
+class FlavourModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "active", "valentines_flavour"]
+
+    class Order:
+        model = Flavour
+
+
+admin.site.register(Flavour, FlavourModelAdmin)
 admin.site.register(PreBuildFlavour)
 admin.site.register(Allergen)
