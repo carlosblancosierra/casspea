ff2dd23c8f45bdbd5ace3a605972ec6ccb10b312
Carlos Blanco <carlosblancosierra@gmail.com>
2024-01-31 18:41:29 +0100

    Feat: NewsletterSubscriber admin view

--- leads/admin.py
+++ leads/admin.py
@@ -3,4 +3,9 @@ from .models import ContactMessage, NewsletterSubscriber
 
 # Register your models here.
 admin.site.register(ContactMessage)
-admin.site.register(NewsletterSubscriber)
+
+@admin.register(NewsletterSubscriber)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['email', 'subscribed_at']
+    list_filter = ['subscribed_at']
+    search_fields = ['subscribed_at',]
